20051aee29698b7ba891429d6b06c5ead63d13d0
Bassman2 <ralf.beckers@hotmail.com>
2021-08-07 18:36:23 +0200

    Update

---
+++ Demo/MidiViewerConsole50/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MidiViewerConsole50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
